2dc05265f285ced531c121aa2374347cbbab78d8
jaya <yourjayaprakash@gmail.com>
2020-04-15 23:14:51 +0800

    refactor to get the elastic connection details from system property

--- kafka-consumer-elasticsearch/src/main/java/com/github/learningtour786/kafka/tutorial/elasticsearch/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/github/learningtour786/kafka/tutorial/elasticsearch/ElasticSearchConsumer.java
@@ -1,6 +1,7 @@
 package com.github.learningtour786.kafka.tutorial.elasticsearch;
 
 import com.google.gson.JsonParser;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -33,35 +34,113 @@ import java.util.Properties;
 
 public class ElasticSearchConsumer {
 
+    Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+
+    private static JsonParser jsonParser = new JsonParser();
+
+    private final String elastic_hostname;
+    private final String elastic_username;
+    private final String elastic_password;
+
     private static String elasticSearchIndexName;
     private static String elasticSearchIndexType;
 
-    public static RestHighLevelClient createClient(){
+    public ElasticSearchConsumer() {
+        // replace with your own credentials
+        /*-Delastic_hostname=<<url>>
+        -Delastic_username=<<username>>
+        -Delastic_password=<<pwd>>*/
+        elastic_hostname = System.getProperty("elastic_hostname"); // localhost or bonsai url
+        elastic_username = System.getProperty("elastic_username"); // needed only for bonsai
+        elastic_password = System.getProperty("elastic_password"); // needed only for bonsai
+        if (StringUtils.isEmpty(elastic_hostname) || StringUtils.isEmpty(elastic_username) || StringUtils.isEmpty(elastic_password)) {
+            throw new RuntimeException("elastic_hostname, elastic_username and elastic_password are required, supply thru system variabble");
+        }
+    }
 
+    public static void main(String[] args) throws IOException {
+        new ElasticSearchConsumer().run();
+    }
+
+    public void run() throws IOException {
+        RestHighLevelClient client = createClient();
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_topic");
+
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
+
+            Integer recordCount = records.count();
+            logger.info("Received " + recordCount + " records");
+
+            BulkRequest bulkRequest = new BulkRequest();
+
+            for (ConsumerRecord<String, String> record : records){
+                // 2 strategies
+                // kafka generic ID
+                // String id = record.topic() + "_" + record.partition() + "_" + record.offset();
+
+                // twitter feed specific id
+                try {
+                    String id = extractIdFromTweet(record.value());
+                    // where we insert data into ElasticSearch
+                    elasticSearchIndexName = "twitter";
+                    elasticSearchIndexType = "tweets";
+                    IndexRequest indexRequest = new IndexRequest(
+                            elasticSearchIndexName,
+                            elasticSearchIndexType,
+                            id // this is to make our consumer idempotent
+                    ).source(record.value(), XContentType.JSON);
+
+                    /*//this block is for indexing single document
+                    IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                    String id1 = indexResponse.getId();
+                    logger.info(id1);*/
+
+                    bulkRequest.add(indexRequest); // we add to our bulk request (takes no time)
+                } catch (NullPointerException e){
+                    logger.warn("skipping bad data: " + record.value());
+                }
+            }
+
+            if (recordCount > 0) {
+                BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                logger.info("Committing offsets...");
+                consumer.commitSync();
+                logger.info("Offsets have been committed");
+
+                bulkItemResponses.iterator().forEachRemaining(bulkItemResponse -> {
+                    logger.info("Index id {}", bulkItemResponse.getId());
+                });
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // close the client gracefully
+        // client.close();
+    }
+
+    public RestHighLevelClient createClient(){
         //////////////////////////
         /////////// IF YOU USE LOCAL ELASTICSEARCH
         //////////////////////////
-
         //  String hostname = "localhost";
         //  RestClientBuilder builder = RestClient.builder(new HttpHost(hostname,9200,"http"));
-
 
         //////////////////////////
         /////////// IF YOU USE BONSAI / HOSTED ELASTICSEARCH
         //////////////////////////
-
-        // replace with your own credentials
-        String hostname = ""; // localhost or bonsai url
-        String username = ""; // needed only for bonsai
-        String password = ""; // needed only for bonsai
-
         // credentials provider help supply username and password
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(username, password));
+                new UsernamePasswordCredentials(elastic_username, elastic_password));
 
         RestClientBuilder builder = RestClient.builder(
-                new HttpHost(hostname, 443, "https"))
+                new HttpHost(elastic_hostname, 443, "https"))
                 .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                     @Override
                     public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
@@ -73,7 +152,7 @@ public class ElasticSearchConsumer {
         return client;
     }
 
-    public static KafkaConsumer<String, String> createConsumer(String topic){
+    public KafkaConsumer<String, String> createConsumer(String topic){
 
         String bootstrapServers = "127.0.0.1:9092";
         String groupId = "kafka-demo-elasticsearch";
@@ -96,81 +175,11 @@ public class ElasticSearchConsumer {
 
     }
 
-    private static JsonParser jsonParser = new JsonParser();
-
     private static String extractIdFromTweet(String tweetJson){
         // gson library
         return jsonParser.parse(tweetJson)
                 .getAsJsonObject()
                 .get("id_str")
                 .getAsString();
-    }
-
-    public static void main(String[] args) throws IOException {
-        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
-        RestHighLevelClient client = createClient();
-
-        KafkaConsumer<String, String> consumer = createConsumer("twitter_topic");
-
-        while(true){
-            ConsumerRecords<String, String> records =
-                    consumer.poll(Duration.ofMillis(100)); // new in Kafka 2.0.0
-
-            Integer recordCount = records.count();
-            logger.info("Received " + recordCount + " records");
-
-            BulkRequest bulkRequest = new BulkRequest();
-
-            for (ConsumerRecord<String, String> record : records){
-
-                // 2 strategies
-                // kafka generic ID
-                // String id = record.topic() + "_" + record.partition() + "_" + record.offset();
-
-                // twitter feed specific id
-                try {
-                    String id = extractIdFromTweet(record.value());
-
-                    // where we insert data into ElasticSearch
-                    elasticSearchIndexName = "twitter";
-                    elasticSearchIndexType = "tweets";
-                    IndexRequest indexRequest = new IndexRequest(
-                            elasticSearchIndexName,
-                            elasticSearchIndexType,
-                            id // this is to make our consumer idempotent
-                    ).source(record.value(), XContentType.JSON);
-
-                    /*//this block is for indexing single document
-                    IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-                    String id1 = indexResponse.getId();
-                    logger.info(id1);*/
-
-                    bulkRequest.add(indexRequest); // we add to our bulk request (takes no time)
-                } catch (NullPointerException e){
-                    logger.warn("skipping bad data: " + record.value());
-                }
-
-            }
-
-            if (recordCount > 0) {
-                BulkResponse bulkItemResponses = client.bulk(bulkRequest, RequestOptions.DEFAULT);
-                logger.info("Committing offsets...");
-                consumer.commitSync();
-                logger.info("Offsets have been committed");
-
-                bulkItemResponses.iterator().forEachRemaining(bulkItemResponse -> {
-                    logger.info("Index id {}", bulkItemResponse.getId());
-                });
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        // close the client gracefully
-        // client.close();
-
     }
 }
